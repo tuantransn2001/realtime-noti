d700576e1aa6cfd4ca5d729661b6f480d6452bb1
Tuấn Trần <tuantransn2001@gmail.com>
2023-11-27 15:29:50 +0700

    init

--- src/notification/notification.gateway.ts
+++ src/notification/notification.gateway.ts
@@ -5,7 +5,7 @@ import {
 } from '@nestjs/websockets';
 import { NotificationService } from './notification.service';
 import { Server, Socket } from 'socket.io';
-import { HttpException, Inject, OnModuleInit } from '@nestjs/common';
+import {  OnModuleInit } from '@nestjs/common';
 import { Notification } from './entities/notification.interface';
 
 interface JwtPayload {
@@ -29,36 +29,12 @@ export class NotificationGateway implements OnModuleInit {
   socketMap = new Map<string, socketMetaPayload>();
 
   constructor(
-    // private readonly authService: AuthService,  // TODO import AuthService if you have one.
+  
     private readonly notificationService: NotificationService,
   ) {}
 
   onModuleInit() {
     this.server.on('connection', async (socket) => {
-      // TODO uncomment this if you plan to have authentication for socket connection.
-      // const token = socket.handshake.headers.authorization?.split(' ')[1];
-      // if (!token) {
-      //   socket.disconnect(true);
-      //   return;
-      // }
-
-      // TODO uncomment this when you have AuthService to verify JWT and get user payload from it.
-      // const payload = await this.authService.verifyJwt(token);
-      // if (!payload) {
-      //   socket.disconnect(true);
-      //   return;
-      // }
-
-      // TODO when authenticated, keep track of the socket connection / user.
-      // this.socketMap.set(payload._id, {
-      //   ...payload,
-      //   socketId: socket.id,
-      // });
-
-      socket.on('disconnect', () => {
-        // TODO uncomment, to remove the user from the socketMap when disconnected.
-        // this.socketMap.delete(socket.id);
-      });
     });
   }
 
